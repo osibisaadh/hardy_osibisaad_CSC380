1dda5702046824fd4cab594f389651dc7fd2cb82
osibisaadh <ohardy@student.neumont.edu>
2013-07-09 19:20:00 -0700

    Deleted bad files as well as updates to client

--- Part 1/Client/src/Client.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: ohardy
- * Date: 7/9/13
- * Time: 5:59 PM
- * To change this template use File | Settings | File Templates.
- */
-public class Client {
-}
--- Part 1/Client/src/Driver.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: ohardy
- * Date: 7/9/13
- * Time: 5:57 PM
- * To change this template use File | Settings | File Templates.
- */
-public class Driver {
-}
