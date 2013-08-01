334697325c4318a27d418bf9dc93253e6ac5229e
osibisaadh <ohardy@student.neumont.edu>
2013-07-31 23:34:16 -0700

    SOAP FOR LUNCH COMPLETE - XML IS IN RESOURCES FOLDER

--- ws_lunch/src/Client/Driver.java
+++ ws_lunch/src/Client/Driver.java
@@ -1,9 +1,20 @@
 package Client;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import Model.Item;
+import Model.Menu;
+import Model.Order;
+import Model.Restaurant;
+import Model.Restaurants;
+import Model.OrderItem;
+import Model.OrderRestaurant;
+import sun.net.www.protocol.http.HttpURLConnection;
+
+import javax.xml.bind.*;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,15 +24,161 @@ import java.net.URLConnection;
  * To change this template use File | Settings | File Templates.
  */
 public class Driver {
-    public static void main(String[] args){
-        try{
-            URL Index = new URL("http://localhost:8080/Index");
-            URLConnection indexConnect = Index.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(indexConnect.getInputStream()));
 
-        }catch(Exception e){
-            e.printStackTrace();
+    private static Unmarshaller unmarshaller;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static boolean menu(){
+        boolean choice = false;
+        boolean valid = false;
+        while(!valid){
+            System.out.println("Would you like to order? y/n");
+            String input = scanner.nextLine();
+            if(input.equalsIgnoreCase("y")){
+                choice = true;
+                valid = true;
+            }else if(input.equalsIgnoreCase("n")){
+                choice = false;
+                valid = true;
+            }
         }
+     return choice;
+    }
+
+    public static void main(String[] args){
+
+        boolean run = true;
+        while(run){
+
+            boolean choice = menu();
+            if(choice){
+
+                try{
+
+                    URL Index = new URL("http://localhost:8080/restaurants");
+                    HttpURLConnection indexConnect = (HttpURLConnection)Index.openConnection();
+                    indexConnect.setRequestMethod("GET");
+                    indexConnect.setRequestProperty("Accept", "text/xml");
+                    InputStream in =indexConnect.getInputStream();
+
+                    JAXBContext jaxbContext = JAXBContext.newInstance("gen");
+                    unmarshaller = jaxbContext.createUnmarshaller();
+
+                    Restaurant restaurant = getRestaurant(in);
+
+                    URL menu = new URL("http://localhost:8080/menu/" + restaurant.getName());
+                    URLConnection menuConnection = menu.openConnection();
+
+                    in =menuConnection.getInputStream();
+
+                    Item item = getItem(in);
+
+                    URL orderURL = new URL("http://localhost:8080/order");
+                    HttpURLConnection orderConnection = (HttpURLConnection)orderURL.openConnection();
+                    orderConnection.setDoOutput(true);
+                    orderConnection.setRequestMethod("PUT");
+
+                    OrderItem orderItem = new OrderItem();
+                    orderItem.setName(item.getName());
+                    orderItem.setPrice(item.getPrice());
+
+                    OrderRestaurant orderRestaurant = new OrderRestaurant();
+                    orderRestaurant.setName(restaurant.getName());
+
+                    Order order = new Order();
+                    order.setOrderItem(orderItem);
+                    order.setOrderRestaurant(orderRestaurant);
+
+                    JAXBContext orderjaxbContext = JAXBContext.newInstance("gen.order");
+                    Marshaller marshaller = orderjaxbContext.createMarshaller();
+                    File xml = new File("order.xml");
+                    marshaller.marshal(order, xml);
+
+                    OutputStream stream = null;
+                    BufferedInputStream buf = null;
+                    try {
+
+                        orderConnection.setDoInput(true);
+                        orderConnection.setDoOutput(true);
+                        orderConnection.setRequestMethod("PUT");
+                        orderConnection.setRequestProperty("Content-Type", "text/xml");
+
+                        orderConnection.setRequestProperty("Content-Length", "" + xml.length());
+                        stream = orderConnection.getOutputStream();
+                        FileInputStream input = new FileInputStream(xml);
+                        buf = new BufferedInputStream(input);
+                        int readBytes = 0;
+                        while ((readBytes = buf.read()) != -1)
+                            stream.write(readBytes);
+                        stream.flush();
+                    } catch (Exception ioe) {
+                        ioe.printStackTrace();
+                    } finally {
+                        if (stream != null)
+                            stream.close();
+                        if (buf != null)
+                            buf.close();
+                    }
+                    orderConnection.connect();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(orderConnection.getInputStream()));
+                    System.out.println(reader.readLine());
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+            else{
+                run = false;
+            }
+
+        }
+    }
+
+    public static Item getItem(InputStream in) throws JAXBException {
+        Item choice = null;
+        Menu menu = (Menu) unmarshaller.unmarshal(in);
+        List<Item> itemList = menu.getItems();
+        boolean valid = false;
+        while(!valid){
+            System.out.println("Choose Item: ");
+            for(int i = 0; i < itemList.size();i++){
+                System.out.println(i + ": " + itemList.get(i).getName());
+            }
+            try{
+                String index = scanner.nextLine();
+                choice = itemList.get(Integer.parseInt(index));
+                valid = true;
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        return choice;
+
+    }
+
+    public static Restaurant getRestaurant(InputStream in) throws JAXBException {
+        Restaurant choice = null;
+        Restaurants restaurants= (Restaurants) unmarshaller.unmarshal(in);
+        List<Restaurant> restaurantList =  restaurants.getRestaurants();
+        System.out.println(restaurantList.size());
+        boolean valid = false;
+        while(!valid){
+            System.out.println("Choose Restaurant: ");
+            for(int i = 0; i < restaurantList.size();i++){
+                System.out.println(i + ": " + restaurantList.get(i).getName());
+            }
+            try{
+                String index = scanner.nextLine();
+                choice = restaurantList.get(Integer.parseInt(index));
+                valid = true;
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        return choice;
+
     }
 
 }
--- ws_lunch/src/sevlets/Order.java
+++ ws_lunch/src/sevlets/Order.java
@@ -1,11 +1,18 @@
 package sevlets;
 
+import DB.RequestHandler;
+import jaxb.oRequest.Envelope;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,12 +21,29 @@ import java.io.IOException;
  * Time: 9:05 PM
  * To change this template use File | Settings | File Templates.
  */
-@WebServlet(name = "Restaurant/*/*")
+@WebServlet("/order")
 public class Order extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String[] route = request.getPathInfo().split("/");
-        String resource = route[route.length-1];
+    RequestHandler requestHandler = RequestHandler.getInstance();
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try{
+            JAXBContext jaxbContext = JAXBContext.newInstance("jaxb.oRequest");
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+            Envelope envelope = (Envelope) unmarshaller.unmarshal(req.getInputStream());
+
+            requestHandler.doAction(envelope.getBody().getCreateOrder());
+
+            System.out.println("Current orders: ");
+            for(Model.Order o : requestHandler.getOrders()){
+                System.out.println("\t" + o.getOrderItem().getName() + " : " + o.getOrderRestaurant().getName());
+            }
+            System.out.println();
+            resp.getWriter().println("Your order has been received.");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
